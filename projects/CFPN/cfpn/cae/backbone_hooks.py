92d2099db36bd55cacea143222760213da1f4d6f
miro <furtadomiro@gmail.com>
2020-04-17 11:18:23 -0400

    .

--- projects/CFPN/cfpn/cae/backbone_hooks.py
+++ projects/CFPN/cfpn/cae/backbone_hooks.py
@@ -21,11 +21,14 @@ class PatchUtil:
 
     @staticmethod
     def batched_patches(images, patch_size):
+        print("Begin batch of {}".format(images.shape))
         patches = images.unfold(-2, patch_size, patch_size)
         patches = patches.unfold(-2, patch_size, patch_size)
         patches = patches.permute(0, 2, 3, 1, -2, -1)
         _, patch_x, patch_y, _, _, _ = patches.shape
-        return patches.permute(0, 2, 3, 1, -2, -1).flatten(0, 2), patch_x, patch_y
+        patches = patches.permute(0, 2, 3, 1, -2, -1).flatten(0, 2)
+        print("Outputting batched size of {}".format(patches.shape))
+        return patches, patch_x, patch_y
 
     @staticmethod
     def reconstruct_from_batched_patches(batched_out, patch_x=4, patch_y=4):
